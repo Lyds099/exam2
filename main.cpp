8cf4d824da1be1665ffd9291ef409c7cd718406d
blair488 <samantha99fan@gmail.com>
2021-05-12 17:57:02 +0800

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -115,8 +115,8 @@ void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
     else if(plot_fig){
         for(int k=0; k<3; k++){
            if(k==0) sprintf(buff, "The classified gesture events");
-           else if(k==1) sprintf(buff, "No 0 1 2 3 4 5 6 7 8 9 10");
-           else if(k==2) sprintf(buff, "ID %d %d %d %d %d %d %d %d %d %d %d",gestures[0],gestures[1],gestures[2],
+           else if(k==1) sprintf(buff, "No 1 2 3 4 5 6 7 8 9 10");
+           else if(k==2) sprintf(buff, "ID %d %d %d %d %d %d %d %d %d %d",gestures[0],gestures[1],gestures[2],
                gestures[3],gestures[4],gestures[5],gestures[6],gestures[7],gestures[8],gestures[9]);
 
            message.qos = MQTT::QOS0;
@@ -132,14 +132,14 @@ void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
         }
         for(int k=0; k<6; k++){
           if(k==0) sprintf(buff, "The extracted features");
-           else if(k==1) sprintf(buff, "No     0  1  2  3  4  5  6  7  8  9  10");
-           else if(k==2) sprintf(buff, "ang= 0 %3d%3d%3d%3d%3d%3d%3d%3d%3d%3d", features[0][0],features[1][0],features[2][0],
+           else if(k==1) sprintf(buff, "No     1   2   3   4   5   6   7   8   9   10");
+           else if(k==2) sprintf(buff, "ang=0  %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d", features[0][0],features[1][0],features[2][0],
            features[3][0],features[4][0],features[5][0],features[6][0],features[7][0],features[8][0],features[9][0]);
-           else if(k==3) sprintf(buff, "ang>0  %3d%3d%3d%3d%3d%3d%3d%3d%3d%3d", features[0][1],features[1][1],features[2][1],
+           else if(k==3) sprintf(buff, "ang>0  %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d", features[0][1],features[1][1],features[2][1],
            features[3][1],features[4][1],features[5][1],features[6][1],features[7][1],features[8][1],features[9][1]);
-           else if(k==4) sprintf(buff, "ang>30 %3d%3d%3d%3d%3d%3d%3d%3d%3d%3d", features[0][2],features[1][2],features[2][2],
+           else if(k==4) sprintf(buff, "ang>30 %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d", features[0][2],features[1][2],features[2][2],
            features[3][2],features[4][2],features[5][2],features[6][2],features[7][2],features[8][2],features[9][2]);
-           else if(k==5) sprintf(buff, "ang>60 %3d%3d%3d%3d%3d%3d%3d%3d%3d%3d", features[0][3],features[1][3],features[2][3],
+           else if(k==5) sprintf(buff, "ang>60 %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d", features[0][3],features[1][3],features[2][3],
            features[3][3],features[4][3],features[5][3],features[6][3],features[7][3],features[8][3],features[9][3]);
 
            message.qos = MQTT::QOS0;
@@ -381,6 +381,7 @@ void AccCapture_mode(){
   //error_reporter->Report("Set up successful...\n");
 
   event_num = 1;
+  memset(features, 0, sizeof(features));
   while (true) {
     // Attempt to read new data from the accelerometer
     got_data = ReadAccelerometer(error_reporter, model_input->data.f,
@@ -426,10 +427,10 @@ void AccCapture_mode(){
       angle_result = atan ((double)a/(double)c) * 180.0 / 3.141592653589793238462;
       if(angle_reference>0) angle_result = angle_result - angle_reference;
       else angle_result = angle_result + abs(angle_reference);
-      if(angle_result==0) features[event_num-1][0] += 1;
-      else if(angle_result>0 && angle_result<30) features[event_num-1][1] += 1;
-      else if(angle_result>=30 && angle_result<60) features[event_num-1][2] += 1;
-      else if(angle_result>=60 && angle_result<90) features[event_num-1][3] += 1;
+      if(angle_result==0.0) features[event_num-1][0] += 1;
+      else if(angle_result>0.0 && angle_result<30.0) features[event_num-1][1] += 1;
+      else if(angle_result>=30.0 && angle_result<60.0) features[event_num-1][2] += 1;
+      else if(angle_result>=60.0 && angle_result<90.0) features[event_num-1][3] += 1;
     }
     if(event_num>=11){
         plot_fig = 1;
